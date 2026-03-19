d6c16da2785219d7027da151d47b259e9add7cfc
soochang <changsiuuuu@gmail.com>
2026-03-19 10:14:21 +0900

    Str3 완성함.

--- Step04_Str3.py
+++ Step04_Str3.py
@@ -19,8 +19,9 @@ body:
 # 검색 혹은 ai 의 도움을 받아서 info에 들어있는 문자열을 dict type으로 바꾸기.
 # dict에 들어있는 내용을 확인하고 다시 yaml 문자열 형식으로 변환하기.
 
+#yaml 형식의 문자열을 safe_load() 로딩해서 dict type으로 변경
 result = yaml.safe_load(info)
-
+print(result)
 print(result["name"])
 print(result["addr"])
 print(result["foods"])
@@ -29,5 +30,9 @@ print(result["foods"][1])
 print(result["body"]["weight"])
 print(result["body"]["height"])
 
-result2 = yaml.dump(result)
+# dict type result를 다시 yaml형식으로 변환하는 dump()
+result2 = yaml.dump(result, allow_unicode=True, sort_keys=False)
+print(result2)
+print(type(result))
+print(type(result2))
 print("종료")
